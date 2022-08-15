741d3d424297e04ee8c23703be159b75ca20b40b
Maxim Startsev <0960746@mail.ru>
2022-08-16 00:00:30 +0300

    Applied changes in myfeature branch

--- m16/ViewController.swift
+++ m16/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        print("myfeature")
         // Do any additional setup after loading the view.
     }
 
