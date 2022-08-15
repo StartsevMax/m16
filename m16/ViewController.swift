cb13415432bb950e12bbd44c8548da471500aa3c
Maxim Startsev <0960746@mail.ru>
2022-08-15 23:14:33 +0300

    Initial Commit

---
+++ m16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m16
+//
+//  Created by Максим Старцев on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
